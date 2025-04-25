0390571cbd3fcccb9bb914191f32ce327cb8fb40
vedanshupatel15 <987561@win.edu.au>
2025-04-25 19:55:43 +0530

    Add files via upload
    Service page

---
+++ service.php
@@ -0,0 +1,99 @@
+<?php include "header.php" ?>
+<section class="page-title bg-1">
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="block text-center">
+          <span class="text-white">Our Services</span>
+          <h1 class="text-capitalize mb-5 text-lg">What We Do</h1>
+					<ul class="list-inline breadcumb-nav">
+            <li class="list-inline-item"><a href="index.php" class="text-white">Home</a></li>
+            <li class="list-inline-item"><span class="text-white">/</span></li>
+            <li class="list-inline-item"><a href="#" class="text-white-50">Our services</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="section service-2">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-6 col-sm-6">
+				<div class="service-block mb-5">
+					<img src="images/service/service-1.jpg" alt="" class="img-fluid">
+					<div class="content">
+						<h4 class="mt-4 mb-2 title-color">Appointment Scheduling</h4>
+						<p class="mb-4">Patients can easily schedule, reschedule, or cancel appointments with mental health professionals.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 col-sm-6">
+				<div class="service-block mb-5">
+					<img src="images/service/service-2.jpg" alt="" class="img-fluid">
+					<div class="content">
+						<h4 class="mt-4 mb-2  title-color">Virtual Consultations</h4>
+						<p class="mb-4">The application supports secure and private video conferencing to enable virtual therapy or counseling sessions.</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-4 col-md-6 col-sm-6">
+				<div class="service-block mb-5">
+					<img src="images/service/service-3.jpg" alt="" class="img-fluid">
+					<div class="content">
+						<h4 class="mt-4 mb-2 title-color">Educational Resources</h4>
+						<p class="mb-4">A curated library of mental health content is available, Guided meditation and mindfulness exercises.</p>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="col-lg-4 col-md-6 col-sm-6">
+				<div class="service-block mb-5 mb-lg-0">
+					<img src="images/service/service-4.jpg" alt="" class="img-fluid">
+					<div class="content">
+						<h4 class="mt-4 mb-2 title-color">Assessment Tools</h4>
+						<p class="mb-4">Built-in self-assessment questionnaires help users understand their current mental health status.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6 col-sm-6">
+				<div class="service-block mb-5 mb-lg-0">
+					<img src="images/service/service-6.jpg" alt="" class="img-fluid">
+					<div class="content">
+						<h4 class="mt-4 mb-2 title-color">Progress Tracking</h4>
+						<p class="mb-4">Patients and doctors can view and analyze progress over time using charts, notes, and symptom tracking.</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-4 col-md-6 col-sm-6">
+				<div class="service-block mb-5 mb-lg-0">
+					<img src="images/service/service-8.jpg" alt="" class="img-fluid">
+					<div class="content">
+						<h4 class="mt-4 mb-2 title-color">Personalized Treatment Plans</h4>
+						<p class="mb-4">Doctors can create, assign, and update treatment plans tailored to each patient's needs.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section cta-page">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-7">
+				<div class="cta-content">
+					<div class="divider mb-4"></div>
+					<h2 class="mb-5 text-lg">We are pleased to offer you the <span class="title-color">chance to have the healthy</span></h2>
+					<a href="appoinment.html" class="btn btn-main-2 btn-round-full">Get appoinment<i class="icofont-simple-right  ml-2"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include "footer.php" ?>
