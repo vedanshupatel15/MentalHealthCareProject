1eeb929795408faaff24a14b02d6f81d52e8d3ad
vedanshupatel15 <987561@win.edu.au>
2025-04-25 19:47:43 +0530

    Add files via upload
    about page

---
+++ about.php
@@ -0,0 +1,173 @@
+<?php include "header.php" ?>
+<section class="page-title bg-1">
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="block text-center">
+          <span class="text-white">About Us</span>
+          <h1 class="text-capitalize mb-5 text-lg">About Us</h1>
+
+          <ul class="list-inline breadcumb-nav">
+            <li class="list-inline-item"><a href="index.php" class="text-white">Home</a></li>
+            <li class="list-inline-item"><span class="text-white">/</span></li>
+            <li class="list-inline-item"><a href="#" class="text-white-50">About Us</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="section about-page">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+				<h2 class="title-color">Compassionate Care for Your Mental Wellness</h2>
+			</div>
+			<div class="col-lg-8">
+				<p>At Virzen Wellness, we believe that mental health is just as important as physical health. Our mission is to make mental healthcare accessible, personalized, and stigma-free for everyone. With the rising need for emotional support and professional guidance, we provide a secure, user-friendly platform where patients can connect with licensed mental health professionals from the comfort of their homes.</p>
+        <p>Our team is dedicated to creating a space where individuals can find hope, healing, and the support they need to thrive. Whether you're struggling with anxiety, depression, stress, or simply need someone to talk to, Virzen Wellness is here for you.</p>
+				<img src="images/about/sign.png" alt="" class="img-fluid">
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="fetaure-page ">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-3 col-md-6">
+        <div class="about-block-item mb-5 mb-lg-0">
+          <img src="images/about/about-1.jpg" alt="" class="img-fluid w-100">
+          <h4 class="mt-3">Mental Healthcare</h4>
+          <p>We offer mental health support for all people ensuring that care is tailored to every stage of life.</p>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <div class="about-block-item mb-5 mb-lg-0">
+          <img src="images/about/about-2.jpg" alt="" class="img-fluid w-100">
+          <h4 class="mt-3">Counseling & Therapy</h4>
+          <p>Access professional help from licensed therapists and counselors via secure online consultations.</p>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <div class="about-block-item mb-5 mb-lg-0">
+          <img src="images/about/about-3.jpg" alt="" class="img-fluid w-100">
+          <h4 class="mt-3">Progress Tracking Tools</h4>
+          <p>Monitor your mental health journey with our interactive dashboards, and mood tracking.</p>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6">
+        <div class="about-block-item">
+          <img src="images/about/about-4.jpg" alt="" class="img-fluid w-100">
+          <h4 class="mt-3">Qualified Doctors</h4>
+          <p>Professionals includes psychologists, psychiatrists, and counselors.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="section awards">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-4">
+        <h2 class="title-color">Our Doctors achievements </h2>
+        <div class="divider mt-4 mb-5 mb-lg-0"></div>
+      </div>
+      <div class="col-lg-8">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="award-img">
+              <img src="images/about/3.png" alt="" class="img-fluid">
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="award-img">
+              <img src="images/about/4.png" alt="" class="img-fluid">
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="award-img">
+              <img src="images/about/1.png" alt="" class="img-fluid">
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="award-img">
+              <img src="images/about/2.png" alt="" class="img-fluid">
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="award-img">
+              <img src="images/about/5.png" alt="" class="img-fluid">
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="award-img">
+              <img src="images/about/6.png" alt="" class="img-fluid">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="section team">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-6">
+        <div class="section-title text-center">
+          <h2 class="mb-4">Meet Our Specialist</h2>
+          <div class="divider mx-auto my-4"></div>
+          <p>Today’s users expect effortless experiences. Don’t let essential people and processes stay stuck in the past. Speed it up, skip the hassles</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="team-block mb-5 mb-lg-0">
+          <img src="images/team/1.jpg" alt="" class="img-fluid w-100">
+
+          <div class="content">
+            <h4 class="mt-4 mb-0"><a href="doctor-single.html">Dr. John Marshal</a></h4>
+            <p>Clinical Psychologist, Trauma Specialist</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="team-block mb-5 mb-lg-0">
+          <img src="images/team/2.jpg" alt="" class="img-fluid w-100">
+
+          <div class="content">
+            <h4 class="mt-4 mb-0"><a href="doctor-single.html">Dr. Marshal Root</a></h4>
+            <p>Psychiatrist, Mood Disorder Specialist</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="team-block mb-5 mb-lg-0">
+          <img src="images/team/3.jpg" alt="" class="img-fluid w-100">
+
+          <div class="content">
+            <h4 class="mt-4 mb-0"><a href="doctor-single.html">Dr. Siamon John</a></h4>
+            <p>Therapist, Cognitive Behavioral Therapy (CBT) Expert</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="team-block">
+          <img src="images/team/4.jpg" alt="" class="img-fluid w-100">
+
+          <div class="content">
+            <h4 class="mt-4 mb-0"><a href="doctor-single.html">Dr. Rishat Ahmed</a></h4>
+            <p>Child & Adolescent Psychiatrist</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php include "footer.php" ?>
