2d2023444b4684f80174877882c72ef4eeda9091
vedanshupatel15 <987561@win.edu.au>
2025-04-25 19:53:11 +0530

    Add files via upload
    Department page

---
+++ department.php
@@ -0,0 +1,99 @@
+<?php include "header.php" ?>
+<section class="page-title bg-1">
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="block text-center">
+          <span class="text-white">All Department</span>
+          <h1 class="text-capitalize mb-5 text-lg">Care Department</h1>
+
+<ul class="list-inline breadcumb-nav">
+            <li class="list-inline-item"><a href="index.php" class="text-white">Home</a></li>
+            <li class="list-inline-item"><span class="text-white">/</span></li>
+            <li class="list-inline-item"><a href="#" class="text-white-50">All Department</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="section service-2">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-7 text-center">
+				<div class="section-title">
+					<h2>Award winning patient care</h2>
+					<div class="divider mx-auto my-4"></div>
+					<p>Lets know moreel necessitatibus dolor asperiores illum possimus sint voluptates incidunt molestias nostrum laudantium. Maiores porro cumque quaerat.</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-4 col-md-6 ">
+				<div class="department-block mb-5">
+					<img src="images/service/service-1.jpg" alt="" class="img-fluid w-100">
+					<div class="content">
+						<h4 class="mt-4 mb-2 title-color">General Adult Psychiatry</h4>
+						<p class="mb-4">Core services for depression, anxiety, bipolar disorder, etc.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6">
+				<div class="department-block mb-5">
+					<img src="images/service/service-2.jpg" alt="" class="img-fluid w-100">
+					<div class="content">
+						<h4 class="mt-4 mb-2  title-color">Child and Adolescent Psychiatry</h4>
+						<p class="mb-4">Specialized services for kids and teens with emotional or developmental challenges.</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-4 col-md-6">
+				<div class="department-block mb-5">
+					<img src="images/service/service-3.jpg" alt="" class="img-fluid w-100">
+					<div class="content">
+						<h4 class="mt-4 mb-2 title-color">Geriatric Psychiatry</h4>
+						<p class="mb-4">Focused on older adults—often involving dementia, Alzheimer's, late-life depression.</p>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="col-lg-4 col-md-6 ">
+				<div class="department-block  mb-5 mb-lg-0">
+					<img src="images/service/service-4.jpg" alt="" class="img-fluid w-100">
+					<div class="content">
+						<h4 class="mt-4 mb-2 title-color">Addiction Psychiatry</h4>
+						<p class="mb-4">Treats substance use disorders with medication, therapy, and detox programs.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6">
+				<div class="department-block mb-5 mb-lg-0">
+					<img src="images/service/service-6.jpg" alt="" class="img-fluid w-100">
+					<div class="content">
+						<h4 class="mt-4 mb-2 title-color">Emergency Psychiatry</h4>
+						<p class="mb-4">Crisis units or emergency rooms with mental health stabilization services.</p>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-4 col-md-6">
+				<div class="department-block mb-5 mb-lg-0">
+					<img src="images/service/service-8.jpg" alt="" class="img-fluid w-100">
+					<div class="content">
+						<h4 class="mt-4 mb-2 title-color">Consult-Liaison Psychiatry</h4>
+						<p class="mb-4">Psychiatric support for patients being treated in other departments (e.g., cancer, trauma).</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include "footer.php" ?>
